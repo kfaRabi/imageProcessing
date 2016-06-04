d49d423a746ad7dba7393c19baec98c1e6bd074b
rabi <fozleazimrabi2@yahoo.com>
2016-06-04 20:35:27 +0600

    Function to flip an image added.

--- src/imagepro/LandingFXMLController.java
+++ src/imagepro/LandingFXMLController.java
@@ -46,7 +46,7 @@ public class LandingFXMLController implements Initializable {
     Image image = null;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        vBoxButtonGroup.setDisable(true);
     }    
 
     @FXML
@@ -60,51 +60,31 @@ public class LandingFXMLController implements Initializable {
         imageOriginal = new Image(file.toURI().toString());
         //int w = (int)imageView.getFitWidth();
         //int h = (int)imageView.getFitHeight();
-        image = SwingFXUtils.toFXImage(colorToGray(imageOriginal), null);
+        image = SwingFXUtils.toFXImage(flip(imageOriginal), null);
         imageView.setImage(image);
         //imageView.setFitHeight(h);
         //imageView.setFitWidth(w);
         //imageView.setPreserveRatio(true);
         centerImage();
         System.out.println("image set");
+        vBoxButtonGroup.setDisable(false);
+        afterButton.setDisable(true);
     }
-    private BufferedImage colorToGray(Image img){
-        BufferedImage bimg = SwingFXUtils.fromFXImage(img, null);
-        int height = bimg.getHeight();
-        int width = bimg.getWidth();
-        
-        //System.out.printf("[%d x %d]\n", width, height);
 
-        for (int c = 0; c < width; c++)
-            for (int r = 0; r < height; r++) {
-                int rgb = bimg.getRGB(c, r);
-                int red   = (rgb >> 16) & 0xFF;
-                int green = (rgb >>  8) & 0xFF;
-                int blue  = (rgb >>  0) & 0xFF;
-
-                int average = (int) (0.21 * red + 0.72 * green + 0.07 * blue);
-                //int average = green;
-                int rr = average;
-                int gg = average;
-                int bb = average;
-//                if (red > 200)
-//                    rr = (int) (red * 1.5);
-//                if (rr > 255)
-//                    rr = 255;
-                rgb = (rr << 16) | (gg << 8) | bb;
-
-                bimg.setRGB(c, r, rgb);
-            }
-        
-
-        return bimg;
-    }
     @FXML
     private void beforeButtonAction(ActionEvent event) {
+        imageView.setImage(imageOriginal);
+        centerImage();
+        beforeButton.setDisable(true);
+        afterButton.setDisable(false);
     }
 
     @FXML
     private void afterButtonAction(ActionEvent event) {
+        imageView.setImage(image);
+        centerImage();
+        beforeButton.setDisable(false);
+        afterButton.setDisable(true);
     }
     
     public void centerImage() {
@@ -132,6 +112,79 @@ public class LandingFXMLController implements Initializable {
         }
     }
     
+    private BufferedImage colorToGray(Image img){
+        BufferedImage bimg = SwingFXUtils.fromFXImage(img, null);
+        int height = bimg.getHeight();
+        int width = bimg.getWidth();
+
+        for (int c = 0; c < width; c++){
+            for (int r = 0; r < height; r++) {
+                int rgb = bimg.getRGB(c, r);
+                int red   = (rgb >> 16) & 0xFF;
+                int green = (rgb >>  8) & 0xFF;
+                int blue  = (rgb >>  0) & 0xFF;
+
+                int average = (int) (0.21 * red + 0.72 * green + 0.07 * blue);
+                //int average = green;
+                int rr = average;
+                int gg = average;
+                int bb = average;
+//                if (red > 200)
+//                    rr = (int) (red * 1.5);
+//                if (rr > 255)
+//                    rr = 255;
+                rgb = (rr << 16) | (gg << 8) | bb;
+
+                bimg.setRGB(c, r, rgb);
+            }
+        }
+        return bimg;
+    }
+    
+    private BufferedImage flip(Image img){
+        BufferedImage bimg = SwingFXUtils.fromFXImage(img, null);
+        BufferedImage flipped = drawWhiteCanvas(img);
+        
+        int height = bimg.getHeight();
+        int width = bimg.getWidth();
+        
+        //System.out.printf("[%d x %d]\n", width, height);
+        
+        int fr = height-1, fc = width-1;
+
+        for (int c = 0; c < width; c++){
+            for (int r = 0; r < height; r++){
+                int rgb = bimg.getRGB(c, r);
+                flipped.setRGB(fc - c, r, rgb);
+                //System.out.println("when c = "+c+", fc - c = "+(fc-c));
+                //fr--;
+            }
+            //fr = height-1;
+            //fc--;
+        }
+        return flipped;
+    }
+    private BufferedImage drawWhiteCanvas(Image img){
+        BufferedImage bimg = SwingFXUtils.fromFXImage(img, null);
+        BufferedImage whiteImage = bimg;
+        
+        int height = bimg.getHeight();
+        int width = bimg.getWidth();
+        
+        int nr = height-1, nc = width-1;
+
+        for (int c = 0; c < width; c++){
+            for (int r = 0; r < height; r++){
+                int rr = 255;
+                int gg = 255;
+                int bb = 255;
+
+                int rgb = (rr << 16) | (gg << 8) | bb;
+                whiteImage.setRGB(nc - c, r, rgb);
+            }
+        }
+        return whiteImage;
+    }    
 }
 
 /* buffered image to javafx image ***** alternative: SwingFXUtils
